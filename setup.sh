973aee2d4bbc2a7ae599ded22729c7fdb138bb44
Xyza Rivera <xyzarivera@gmail.com>
2023-10-21 21:52:26 +0900

    add setup script

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env zsh
+
+# files to symlink
+files=()
+
+# symlink-ing
+for f in $files; do
+  dest="$HOME/.$f"
+
+  #fails if file exists
+  #check prev file is symlinked
+  # -L checks file is a symlink
+  if [[ -L "$dest" ]]; then
+    # we unlink instead of rm -rf to avoid unwanted deletion of actual source
+    # unlink is deleteion of symlink file
+    unlink "$dest"
+
+    # -f checks if provided path is a file
+    # -d is a directory 
+  elif [[ -f "$dest" || -d "$dest" ]]; then
+    echo "Failed to symlink $dest; Non symlinked file/dir exists"
+    continue
+  fi
+
+  # ln = link
+  # -s = symbolic
+  ln -s "$PWD/$f" "$dest"
+done
