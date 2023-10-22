83505cb4dedc53010de175cf17c0d0e979991445
Xyza Rivera <xyzarivera@gmail.com>
2023-10-22 23:57:08 +0900

    add iniital neovim config

---
+++ config/nvim/init.lua
@@ -0,0 +1,28 @@
+vim.g.mapleader = " "
+--disable autoloading vimconfig
+vim.opt.exrc = false 
+
+-- SEARCH
+-- always set to global search
+vim.opt.gdefault = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- enable regex
+vim.opt.magic = true
+
+-- spaces
+--
+vim.opt.expandtab = true
+vim.opt.linebreak = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+vim.opt.wrap = false
+vim.opt.shiftround = true
+
+-- INTERFACE
+vim.opt.number = true
+vim.opt.numberwidth = 4
+vim.opt.startofline = true
