b39edf4ba84480c7595585a46b2617ef79c1184d
Xyza Rivera <xyzarivera@gmail.com>
2025-01-14 11:52:01 +0900

    add git blame in nvim git plugin

--- config/nvim/lua/xyz/plugins/git.lua
+++ config/nvim/lua/xyz/plugins/git.lua
@@ -13,6 +13,16 @@ return {
         changedelete = { text = "▎" },
         untracked = { text = "▎" },
       },
+      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+        delay = 1000,
+        ignore_whitespace = false,
+        virt_text_priority = 100,
+        use_focus = true,
+      },
+      current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
       on_attach = function(buffer)
         local gs = package.loaded.gitsigns
 
