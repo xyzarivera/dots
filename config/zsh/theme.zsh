c98cd68851ab6734a1de7c3cc2fc08f9264200ff
Xyza Rivera <xyzarivera@gmail.com>
2025-06-20 15:53:30 +0900

    (zsh) add comments for zsh theme

--- config/zsh/theme.zsh
+++ config/zsh/theme.zsh
@@ -1,4 +1,7 @@
+# http://zsh.sourceforge.io/Doc/Release/Prompt-Expansion.html
 autoload -U colors && colors
+
+# enable parameter expansion, command substitution and arithmetic expansion
 setopt prompt_subst
 
 PROMPT='%B%(?.%F{blue}>.%F{red}>)%f%b '
