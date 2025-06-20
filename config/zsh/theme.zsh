f4daac1bbd530b1c30ac89e13de06efba5ae9cd3
Xyza Rivera <xyzarivera@gmail.com>
2025-06-20 15:54:09 +0900

    (zsh) add git branch display in theme

--- config/zsh/theme.zsh
+++ config/zsh/theme.zsh
@@ -1,7 +1,14 @@
 # http://zsh.sourceforge.io/Doc/Release/Prompt-Expansion.html
 autoload -U colors && colors
 
+# vsc - git
+# https://hzsh.sourceforge.io/Doc/Release/User-Contributions.html#Version-Control-Information
+autoload -Uz vcs_info
+precmd() { vcs_info }
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:git:*' formats 'on %F{red}%b%f '
+
 # enable parameter expansion, command substitution and arithmetic expansion
 setopt prompt_subst
 
-PROMPT='%B%(?.%F{blue}>.%F{red}>)%f%b '
+PROMPT='%F{blue}%~%f ${vcs_info_msg_0_}%B%(?.%F{blue}>.%F{red}>)%f%b '
