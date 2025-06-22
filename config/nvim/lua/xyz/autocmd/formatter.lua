b3b7724e03fba0c43a01ca94ae2d826fda6cab36
Xyza Rivera <xyzarivera@gmail.com>
2025-06-22 23:48:38 +0900

    (lsp) add svelte to autoformatted files

--- config/nvim/lua/xyz/autocmd/formatter.lua
+++ config/nvim/lua/xyz/autocmd/formatter.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = { "*.js", "*.cjs", "*.mjs", "*.jsx", "*.ts", "*.tsx", "*.vue" },
+  pattern = { "*.js", "*.cjs", "*.mjs", "*.jsx", "*.ts", "*.tsx", "*.vue", "*.svelte" },
   callback = function()
     if vim.fn.exists(":EslintFixAll") ~= 0 then
       vim.cmd("EslintFixAll")
