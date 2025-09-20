bf265fbd9e4dd0608509e47da3645450405f0f82
Xyza Rivera <xyzarivera@gmail.com>
2025-09-20 14:44:32 +0900

    (nvim) install dadbod for sql integration in nvim

--- config/nvim/lua/xyz/plugins/init.lua
+++ config/nvim/lua/xyz/plugins/init.lua
@@ -36,6 +36,7 @@ lazy.setup({
     require("xyz.plugins.cmp"),
     require("xyz.plugins.lsp"),
     require("xyz.plugins.format"),
+    require("xyz.plugins.db"),
   },
   install = {
     missing = true,
