2ed0079a88d11e6cfce0b5bfc94466cfe5c158ae
Xyza Rivera <xyzarivera@gmail.com>
2026-03-23 00:40:26 +0900

    feat: move which-key

--- config/nvim-12/plugins/11_ui.lua
+++ config/nvim-12/plugins/11_ui.lua
@@ -2,10 +2,37 @@ vim.pack.add({
   {
     src = _G.xyz.gh("folke/tokyonight.nvim"),
     name = "tokyonight",
-  }
+  },
+  _G.xyz.gh("folke/which-key.nvim")
 })
 
 -- colorscheme
 require("tokyonight")
 vim.cmd("colorscheme tokyonight-storm")
 
+-- whichkey
+local wk = require("which-key")
+vim.opt.timeout = true
+vim.opt.timeoutlen = 300
+
+wk.setup({
+  plugins = { spelling = true },
+})
+
+wk.add({
+  {
+    mode = { "n", "v" },
+    { "<leader><tab>", group = "tab" },
+    { "<leader>c", group = "code" },
+    { "<leader>f", group = "files" },
+    { "<leader>g", group = "git" },
+    { "<leader>gh", group = "hunks" },
+    { "<leader>l", group = "lsp" },
+    { "<leader>s", group = "search" },
+    { "<leader>v", group = "nvim" },
+    { "<leader>x", group = "quickfix/loclist" },
+    { "[", group = "prev" },
+    { "]", group = "next" },
+    { "g", group = "goto" },
+  },
+})
