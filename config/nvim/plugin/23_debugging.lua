c3a4396f3ecf3a145c722b4f7afeeed16de9767a
Xyza Rivera <xyzarivera@gmail.com>
2026-03-30 14:29:41 +0900

    refactor: move diagnostic related keybinds to debugging config

--- config/nvim/plugin/23_debugging.lua
+++ config/nvim/plugin/23_debugging.lua
@@ -1,14 +1,46 @@
+local open_diagnostic_virtual_line = function()
+  vim.diagnostic.config({
+    virtual_lines = { current_line = true },
+    virtual_text = false,
+  })
+  _G.xyz.new_autocmd("CursorMoved", "*", function()
+    vim.diagnostic.config(_G.xyz.diagnostic_base_options)
+  end, "Reset diagnostic config", true)
+end
+
+local diagnostic_goto = function(next, severity)
+  severity = severity and vim.diagnostic.severity[severity] or nil
+  return function()
+    vim.diagnostic.jump({ count = next and 1 or -1, severity = severity })
+    vim.schedule(function()
+      open_diagnostic_virtual_line()
+    end)
+  end
+end
+
+_G.xyz.keybind_set(
+  "n",
+  "<leader>cd",
+  open_diagnostic_virtual_line,
+  "View Line Diagnostics"
+)
+_G.xyz.keybind_set("n", "]d", diagnostic_goto(true), "Next Diagnostic")
+_G.xyz.keybind_set("n", "[d", diagnostic_goto(false), "Prev Diagnostic")
+_G.xyz.keybind_set("n", "]e", diagnostic_goto(true, "ERROR"), "Next Error")
+_G.xyz.keybind_set("n", "[e", diagnostic_goto(false, "ERROR"), "Prev Error")
+_G.xyz.keybind_set("n", "]w", diagnostic_goto(true, "WARN"), "Next Warning")
+_G.xyz.keybind_set("n", "[w", diagnostic_goto(false, "WARN"), "Prev Warning")
 local setup_deferred = _G.xyz.deferred_packadd({
   _G.xyz.gh("folke/trouble.nvim"),
-  _G.xyz.gh("folke/todo-comments.nvim")
+  _G.xyz.gh("folke/todo-comments.nvim"),
 })
 
 setup_deferred(function()
--- trouble
+  -- trouble
   require("trouble").setup({
     focus = true,
     auto_close = true,
-    auto_preview = false
+    auto_preview = false,
   })
 
   _G.xyz.keybind_set(
@@ -57,15 +89,29 @@ setup_deferred(function()
     end
   end, "Next trouble/quickfix item")
 
--- todo-comments
+  -- todo-comments
   require("todo-comments").setup({
     signs = false,
   })
 
-  _G.xyz.keybind_set( "n", "]t", function() require("todo-comments").jump_next() end, "Next todo comment" )
-  _G.xyz.keybind_set( "n", "[t", function() require("todo-comments").jump_prev() end, "Previous todo comment" )
-  _G.xyz.keybind_set( "n", "<leader>xt", "<cmd>TodoTrouble<cr>", "Todo (Trouble)" )
-  _G.xyz.keybind_set( "n", "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", "Todo/Fix/Fixme (Trouble)" )
+  _G.xyz.keybind_set("n", "]t", function()
+    require("todo-comments").jump_next()
+  end, "Next todo comment")
+  _G.xyz.keybind_set("n", "[t", function()
+    require("todo-comments").jump_prev()
+  end, "Previous todo comment")
+  _G.xyz.keybind_set(
+    "n",
+    "<leader>xt",
+    "<cmd>TodoTrouble<cr>",
+    "Todo (Trouble)"
+  )
+  _G.xyz.keybind_set(
+    "n",
+    "<leader>xT",
+    "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>",
+    "Todo/Fix/Fixme (Trouble)"
+  )
   -- TODO: replacement?
   -- _G.xyz.keybind_set( "n", "<leader>st", "<cmd>TodoTelescope<cr>", "Todo" )
   -- _G.xyz.keybind_set( "n", "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", "Todo/Fix/Fixme" )
