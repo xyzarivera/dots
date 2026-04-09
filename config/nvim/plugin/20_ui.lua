8e183b51eb3cb8e0d31e07ad509fad73f74df992
Xyza Rivera <xyzarivera@gmail.com>
2026-04-09 14:39:19 +0900

    feat: add lsp-progress plugin

--- config/nvim/plugin/20_ui.lua
+++ config/nvim/plugin/20_ui.lua
@@ -6,6 +6,7 @@ vim.pack.add({
   _G.xyz.gh("folke/which-key.nvim"),
   _G.xyz.gh("stevearc/oil.nvim"),
   _G.xyz.gh("nvim-lualine/lualine.nvim"),
+  _G.xyz.gh("linrongbin16/lsp-progress.nvim"),
 })
 
 -- colorscheme
@@ -47,6 +48,45 @@ require("oil").setup({
 })
 
 _G.xyz.keybind_set("n", "<leader>e", "<CMD>Oil<CR>", "File Explorer")
+
+-- lsp progress
+local lspProgress = require("lsp-progress")
+lspProgress.setup({
+  client_format = function(client_name, spinner)
+    return {
+      name = client_name,
+      body = spinner,
+    }
+  end,
+  format = function(client_messages)
+    local active_clients = {}
+    for _, msg in ipairs(client_messages) do
+      active_clients[msg.name] = msg.body
+    end
+
+    local all_clients = vim.lsp.get_clients()
+    if #all_clients == 0 then
+      return ""
+    end
+
+    table.sort(all_clients, function(a, b)
+      return a.name < b.name
+    end)
+
+    local builder = {}
+    for _, client in ipairs(all_clients) do
+      local spinner = active_clients[client.name]
+      if spinner and spinner ~= "" then
+        table.insert(builder, string.format("%s %s", client.name, spinner))
+      else
+        table.insert(builder, client.name)
+      end
+    end
+
+    local sign = "" -- nf-fa-gear
+    return sign .. " " .. table.concat(builder, ", ")
+  end,
+})
 
 -- lualine
 require("lualine").setup({
